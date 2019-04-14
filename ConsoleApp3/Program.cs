8fd60d2e0d427ec5822c07b89d73d32f78a56baf
Paras Gangwani <parasgangwani292@gmail.com>
2019-04-14 19:10:13 +0530

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection conn = DBUtils.GetDBConnection();
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            try
+            {
+                Query(conn);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                // Close connection.
+                conn.Close();
+                // Dispose object, Freeing Resources.
+                conn.Dispose();
+            }
+
+            Console.Read();
+        }
+
+ 
+
+        private static void Query(MySqlConnection conn)
+        {
+            string sql = "Select name from test1";
+
+            MySqlCommand cmd = new MySqlCommand();
+
+            cmd.Connection = conn;
+            cmd.CommandText = sql;
+
+
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+
+                    while (reader.Read())
+                    {
+                        int nameIndex = reader.GetOrdinal("name"); // 0
+                        string name = reader.GetString(nameIndex);
+                        Console.WriteLine("--------------------");
+                        Console.WriteLine("name:" + name);
+                    }
+                }
+            }
+        }
+         
+    }
+}
